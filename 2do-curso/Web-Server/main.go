5ffb043f396c28bb5d60092da54b6a7a638c5811
Alan Lazo <88849164+C4fesit0@users.noreply.github.com>
2023-09-08 00:56:40 -0300

    Delete 2do-curso directory

--- 2do-curso/Web-Server/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-	server := NewServer(":3000")
-	server.Handle("GET", "/", HandlerRoot)
-	server.Handle("POST", "/user", UserPostRequest)
-	server.Handle("POST", "/api", server.AddMiddleware(HandlerHome, CheckAuth(), Logging()))
-	server.Listen()
-}
